b2bc3689cee72f88338a40d046b8ae67e68b9097
arugnata <puriarugnata@gmail.com>
2022-11-25 10:21:19 +0545

    PHP

---
+++ even0-100.php
@@ -0,0 +1,16 @@
+
+
+<?php
+
+
+for ($_getal = 0; $_getal < 100; $_getal++) {
+
+    if ($_getal % 2 == 0) {
+        echo "$_getal<br>";
+    }
+}
+
+?>
+
+
+
---
+++ forloops.php
@@ -0,0 +1,10 @@
+<?php 
+for ($x = 2; $x <= 100; $x+=2) {
+
+if ($x==60){
+continue;
+        }
+    echo "The number is: $x <br>";
+} 
+?>
+
